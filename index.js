0f384e73e7ecc27856bb786e5283a9d4fa86399d
aliraees655123 <96068596+aliraees655123@users.noreply.github.com>
2023-01-29 00:55:03 +0500

    ss

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const adminRouter = require('./routes/AdminRoutes')
 
 app.use(express.json());
-const expressLayouts = require('express-ejs-layouts');
+// const expressLayouts = require('express-ejs-layouts');
 
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
@@ -18,32 +18,32 @@ app.use("/admin",adminRouter);
 
 
 //////////
-app.use(function (err, req, res, next) {
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
+// app.use(function (err, req, res, next) {
+//     res.locals.message = err.message;
+//     res.locals.error = req.app.get("env") === "development" ? err : {};
   
-    // render the error page
-    res.status(err.status || 500);
-    res.render("error");
-  });
-  app.use(
-    cors({
-      origin: "*",
-      optionsSuccessStatus: 200,
-      credentials: true,
-    })
-  );
-  app.options(
-    "*",
-    cors({
-      origin: "*",
-      optionsSuccessStatus: 200,
-      credentials: true,
-    })
-  );
+//     // render the error page
+//     res.status(err.status || 500);
+//     res.render("error");
+//   });
+//   app.use(
+//     cors({
+//       origin: "*",
+//       optionsSuccessStatus: 200,
+//       credentials: true,
+//     })
+//   );
+//   app.options(
+//     "*",
+//     cors({
+//       origin: "*",
+//       optionsSuccessStatus: 200,
+//       credentials: true,
+//     })
+//   );
 
-  app.set('view engine', 'ejs');
-app.use(expressLayouts);
+//   app.set('view engine', 'ejs');
+// app.use(expressLayouts);
   ///////////
 
 
