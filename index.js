906eb76f0d59ba2d7c2d6759cc01841f3eb28518
aliraees655123 <96068596+aliraees655123@users.noreply.github.com>
2023-01-30 11:12:34 +0500

    ss

--- index.js
+++ index.js
@@ -26,14 +26,14 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 //////////
-app.use(function (err, req, res, next) {
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
+// app.use(function (err, req, res, next) {
+//     res.locals.message = err.message;
+//     res.locals.error = req.app.get("env") === "development" ? err : {};
   
-    // render the error page
-    res.status(err.status || 500);
-    res.render("error");
-  });
+   
+//     res.status(err.status || 500);
+//     res.render("error");
+//   });
   app.use(
     cors({
       origin: "*",
